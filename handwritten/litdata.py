038885360bacf523aba4e68da7b2fd75b9afad49
Aleksandr Uldin <alex.uldin@gmail.com>
2023-07-17 22:12:52 +0200

    handwritten: next step

---
+++ handwritten/litdata.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+import pytorch_lightning as pl
+
+
+class LitData(pl.LightningDataModule):
+    def __init__(self, batch_size):
+        super().__init__()
+        self._batch_size = batch_size
+        self._dims = (1, 28, 28)
+        self._data_dir = Path(__file__).parent / 'data'
+        assert self._data_dir.is_dir()
+        self._dataset_train = None
+
+    @property
+    def dims(self):
+        return self._dims
+
+    def prepare_data(self):
+        MNIST(self._data_dir.as_posix(), train=True, download=True)
+
+    def setup(self, stage: str):
+        assert stage == 'fit'
+        self._dataset_train = MNIST(self._data_dir.as_posix(), train=True, download=False,
+                                    transform=transforms.ToTensor())
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self._dataset_train, batch_size=self._batch_size, num_workers=4)
--- handwritten/litmodel.py
+++ handwritten/litmodel.py
@@ -25,8 +25,8 @@ class Generator(nn.Module):
             nn.Tanh(),
         )
 
-    def forward(self, x: torch.Tensor):
-        img = self._model(x)
+    def forward(self, z: torch.Tensor):
+        img = self._model(z)
         img = img.view(img.size(0), *self._img_shape)
         return img
 
@@ -41,11 +41,64 @@ class Discriminator(nn.Module):
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(256, 1),
-            nn.Sigmoid(),
         )
+
+    def forward(self, x: torch.Tensor):
+        x = x.view(x.size(0), -1)
+        y = self._model(x)
+        return y
 
 
 class LitModel(pl.LightningModule):
-    def __init__(self):
+    def __init__(
+            self,
+            img_shape: tuple[int],
+            latent_dim: int,
+            lr: float,
+            adam_betas: tuple[float] = (.5, .999),
+    ):
         super().__init__()
-    
+        self.save_hyperparameters()
+        self.automatic_optimization = False
+
+        self._generator = Generator(self.hparams.latent_dim, self.hparams.img_shape)
+        self._discriminator = Discriminator(self.hparams.img_shape)
+
+        self._criterion = nn.BCEWithLogitsLoss()
+
+    def forward(self, z: torch.Tensor):
+        return self._generator(z)
+
+    def training_step(self, batch: torch.Tensor):
+        x, _ = batch
+        batch_size = x.size(0)
+
+        z = torch.randn(batch_size, self.hparams.latent_dim)
+
+        opt_g, opt_d = self.optimizers()
+
+        self.toggle_optimizer(opt_g)
+        opt_g.zero_grad()
+        x_g = self(z)
+        y = self._discriminator(x_g)
+        loss_g = self._criterion(y, torch.ones(batch_size, 1))
+        self.manual_backward(loss_g)
+        opt_g.step()
+        self.untoggle_optimizer(opt_g)
+
+        self.toggle_optimizer(opt_d)
+        opt_d.zero_grad()
+        y_r = self._discriminator(x)
+        loss_r = self._criterion(y_r, torch.ones(batch_size, 1))
+        y_g = self._discriminator(x_g.detach())  # !!
+        loss_g = self._criterion(y_g, torch.zeros(batch_size, 1))
+        loss_d = (loss_r + loss_g) / 2
+        self.manual_backward(loss_d)
+        opt_d.step()
+        self.untoggle_optimizer(opt_d)
+
+    def configure_optimizers(self):
+        lr, betas = self.hparams.lr, self.hparams.adam_betas
+        opt_g = torch.optim.Adam(self._generator.parameters(), lr=lr, betas=betas)
+        opt_d = torch.optim.Adam(self._discriminator.parameters(), lr=lr, betas=betas)
+        return [opt_g, opt_d], []
